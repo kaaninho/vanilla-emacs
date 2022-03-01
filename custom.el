3275ede9792a3fa124724193b605600432def166
Kaan Sahin <kaan.sahin@active-group.de>
2022-03-01 12:25:50 +0100

    Add changes to custom.el

--- custom.el
+++ custom.el
@@ -7,11 +7,23 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-safe-themes
+   '("4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" default))
  '(package-selected-packages
-   '(diminish amx smex csv-mode csv mu4e-alert auto-highlight-symbol idle-highlight-mode marginalia projectile clj-refactor cider clojure-mode git-timemachine org-present project counsel swiper lsp-ivy lsp-ui-mode lsp-ui elixir-mode elixir lsp scala lsp-metals sbt-mode org-notify company which-key mu4e rainbow-delimiters paredit markdown-mode)))
+   '(keycast org-superstar doom-themes org-bullets diminish amx smex csv-mode csv mu4e-alert auto-highlight-symbol idle-highlight-mode marginalia projectile clj-refactor cider clojure-mode git-timemachine org-present project counsel swiper lsp-ivy lsp-ui-mode lsp-ui elixir-mode elixir lsp scala lsp-metals sbt-mode org-notify company which-key mu4e rainbow-delimiters paredit markdown-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-info ((t (:inherit default :weight bold :foreground "#F8F8F2" :font "DejaVu Sans Mono" :height 1.0 :underline nil))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#333333" :family "Sans Serif" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#333333" :family "Sans Serif" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#333333" :family "Sans Serif" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#333333" :family "Sans Serif" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#333333" :family "Sans Serif" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#333333" :family "Sans Serif"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#333333" :family "Sans Serif"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#333333" :family "Sans Serif"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#333333" :family "Sans Serif"))))
+ '(org-warning ((t (:inherit default :weight bold :foreground "#f2fffc" :font "DejaVu Sans Mono" :height 1.05 :underline nil)))))
