a1d3e5bf7244f90238c8fd288fe3d55b609fbb00
Kaan Sahin <kaan.sahin@active-group.de>
2023-05-23 16:00:41 +0200

    Add Racket to org-babel

--- init.el
+++ init.el
@@ -294,6 +294,12 @@
   ;; org-reveal
   (setq org-reveal-root "file:///home/kaan/tmp/reveal.js")
 
+
+  ;; Damit Racket in org-babel funktioniert
+  (add-to-list 'load-path "/home/kaan/.emacs.d/my-elisp-files/")
+  (require 'ob-racket) ;; spits error message "Eager macro-expansion failure: (wrong-number-of-arguments (2 . 2) 0)"
+  (setq org-babel-command:racket "/home/kaan/.nix-profile/bin/racket")
+
   ;; org-babel language support
   (setq org-confirm-babel-evaluate nil)
   (with-eval-after-load 'org
@@ -305,7 +311,8 @@
        (python . t)
        (latex . t)
        (plantuml . t)
-       (scheme . t))))
+       (scheme . t)
+       (racket . t))))
 
   ;; ---- Ende :init ----
 
