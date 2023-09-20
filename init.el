85eeaee53f347ef40df448944e6e1e58ca871796
Kaan Sahin <kaan.sahin@active-group.de>
2023-09-20 17:58:48 +0200

    Add org-roam for zettelkasten

--- init.el
+++ init.el
@@ -415,6 +415,34 @@
   :bind (("C-c a" . org-agenda)
          ("C-c c" . org-capture)))
 
+(use-package org-roam
+  :custom
+  (org-roam-directory (file-truename "/home/kaan/org/org-roam"))
+  (org-roam-dailies-directory "/home/kaan/org/org-roam/daily")
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n d" . org-roam-dailies-goto-today)
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  (setq org-roam-graph-viewer "/usr/bin/google-chrome")
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
+(use-package org-roam-ui
+  :after org-roam
+  :custom
+  (org-roam-ui-sync-theme t)
+  (org-roam-ui-follow t)
+  (org-roam-ui-update-on-save t)
+  (org-roam-ui-open-on-start t))
+
 ;;; Idle Highlight Mode
 (use-package auto-highlight-symbol
   :demand t
