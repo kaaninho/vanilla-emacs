7d8e8d4744908090a9fb1ee5f90a1401eb248a4a
Kaan Sahin <kaaninho@gmail.com>
2025-12-01 18:04:46 +0100

    Deactivate posframe

--- init.el
+++ init.el
@@ -200,13 +200,13 @@
 
 ;;; This is for inlining the Mx- and Search buffers
 ;;  a centered frame appears
-(use-package ivy-posframe
-  :diminish ""
-  :after ivy
-  :config
-  (setq ivy-posframe-border-width 1)
-  (set-face-attribute 'ivy-posframe-border nil :background "#FD971F")
-  (ivy-posframe-mode))
+;; (use-package ivy-posframe
+;;   :diminish ""
+;;   :after ivy
+;;   :config
+;;   (setq ivy-posframe-border-width 1)
+;;   (set-face-attribute 'ivy-posframe-border nil :background "#FD971F")
+;;   (ivy-posframe-mode))
 
 ;;; Counsel
 (use-package counsel
